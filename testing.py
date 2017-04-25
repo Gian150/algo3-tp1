2a4182ff80615cf3b32e3d650ffba1b6613459ca
Zamboni Gianfranco <gzamboni@ws5.labo5>
2017-04-25 16:29:33 -0300

    Empezado el script de tiempos

--- testing.py
+++ testing.py
@@ -1,16 +1,69 @@
-import os, sys, subprocess, json
+import os, sys
+import subprocess as s
+import json
+import random as r
+from ipywidgets import IntProgress, HTML, VBox
+from IPython.display import display
+
+def log_progress(sequence, every=None, size=None):
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(sequence)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)     # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, record in enumerate(sequence, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = '{index} / ?'.format(index=index)
+                else:
+                    progress.value = index
+                    label.value = u'{index} / {size}'.format(
+                        index=index,
+                        size=size
+                    )
+            yield record
+    except:
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = index
+        label.value = str(index or '?')
+
 
 bigTests = ["t20", "t16", "t17", "t15", "t4", "t18", "t19"]
 
 def testCorrectitud(executable, solutions):
-	for test in os.listdir("Tests"):
+	for test in os.listdir("Tests_para_correctitud"):
 		t = open("Tests/" + test, "r")
 		
 		if (executable == "./Ej1" or executable == "./Ej2") and (test in bigTests):
 			print(test + "... es demasiado grande para esta aplicación")
 		else:
 			print(test + "....", end="")
-			out = subprocess.check_output([executable], stdin=t, shell=True)
+			out = s.check_output([executable], stdin=t, shell=True)
 			if(int(out) == solutions[test]):
 				print("OK", flush=True)
 			else:
@@ -33,27 +86,51 @@ def correctitud(whatToTest):
 	
 	if(whatToTest == "d" or whatToTest == "a"):
 		print("Checking dynamic algorithm")
-		print("=================================")
+		progressrint("=================================")
 		testCorrectitud("./Ej3", realSolutions)
 
-def testAlgorithms():
-	correctitud()
 
+def generateRandomSamples():
+	for n in range(1, 200):
+		for repetition in log_progress(range(0, 20)):
+			f = open("Tests_para_tiempos/Test_" + str(n) + "_" + str(repetition), "w")
+			f.write(str(n) + "\n")
+			for i in range(0, n):
+				f.write(str(r.randrange(-150,150)) + " ")
+			f.close()
+
+def timing(whatToTest):
+	if len(os.listdir("Tests_para_tiempos")) == 0:
+		print("Creating tests files")
+		generateRandomSamples()
+	print("Test files generated")
+
+#	if(whatToTest == "b" or whatToTest == "a")
+#		print("Running backtracking iterations")
+#		print("===============================")
 
 if __name__ == "__main__":
 	
 	if len(sys.argv)  == 1:
-		print("Debe suministra uno o mas parametros: \n")
-		print("\t cor - Para testear correctitud")
-		print("\t\t  a (default) - de los 3 algoritmos")
-		print("\t\t  b 			 - del Backtracking")
-		print("\t\t  bb 		 - del Backtracking con podas")
-		print("\t\t  d 			 - del de programación Dinamica")
-		print("\t tim - Para testear tiempos")	
-	elif sys.argv[1] == "cor":
+		print("You must provide two or more parameters: \n")
+		print("\t test -To test algorithms")
+		print("\t\t  a (default) - test the three algorithms")
+		print("\t\t  b 			 - test only Backtracking")
+		print("\t\t  bb 		 - test only Backtracking with Bounds")
+		print("\t\t  d 			 - test only dynamic algorithm")
+		print("\t tim - To take execution times:")	
+		print("\t\t  a (default) - of every algorithm")
+		print("\t\t  b 			 - of the Backtracking algorithm")
+		print("\t\t  bb 		 - of the Backtracking with Bounds algorithm")
+		print("\t\t  d 			 - of the dynamic algorithm")
+		
+	elif sys.argv[1] == "test":
 		if len(sys.argv) == 3:
 			correctitud(sys.argv[2])
 		else:
 			correctitud("a")
-#	else:
-#		testearTiempos()
+	elif sys.argv[1] == "tim":
+		if len(sys.argv) == 3:
+			timing(sys.argv[2])
+		else:
+			timing("a")
